f5c4347c890e1f731b0ed3fe8e2682ed29e19574
cfabric <christopher.frauenberger@tuwien.ac.at>
2015-06-12 13:15:23 +0000

    reading files in python, pics still distorted

--- PiDev/btmonitor.py
+++ PiDev/btmonitor.py
@@ -18,17 +18,26 @@ while command!='$HSEnd':
 		data = ''
 		data = sock.recv(1024)
 		buf = buf + data
+#	print "buf: [%s]" % buf
 	if buf.find('$HSStart') != -1:
 		command = '$HSStart'
 	if buf.find('$HSEnd') != -1:
 		command = '$HSEnd'
 	if buf.find('$HSFile:') != -1:
-		filename = buf[8:buf.find('-end')]
-		print filename
-#		f = open('import/' + filename, 'w');
-		
-
-#	print "buf: [%s]" % buf
+		filename = buf[8:buf.find('|')]
+		size = int(buf[buf.find('|')+1:buf.find('-end')])
+		print filename, size,
+		f = open('import/' + filename, 'wb+')
+		while size > 0:
+			if size > 1024:
+				data = sock.recv(1024)
+			else:
+				data = sock.recv(size)
+			size -= len(data)
+			f.write(data)
+#			print size
+		print "  Written to file"
+		f.close()
 
 
 print 'Connection closed'
